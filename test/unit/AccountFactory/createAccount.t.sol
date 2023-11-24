c13c10fdc92ea98897d73615fb6186efa0042679
qd-qd <qdqdqdqdqd@protonmail.com>
2023-11-24 20:47:01 +0100

    ✨ develop the multi-steps create account flow
    This commit implements the function to create an account without
    initializing it. This function can be used in a multi-steps flow
    where the account is created first and then initialized later.
    In addition to the unit tests, a new integration test has been added
    to garantee the deterministic deployment of the account matches the
    formulas defined in the getAddress function.
    It's important to note this function deploy a ERC1967Proxy for the user
    using the provided login hash.
    Also, a script has been written to call the function from the CLI.

---
+++ test/unit/AccountFactory/createAccount.t.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.20 <0.9.0;
+
+import { AccountFactory } from "src/AccountFactory.sol";
+import { BaseTest } from "test/BaseTest.sol";
+
+contract AccountFactory__CreateAccount is BaseTest {
+    AccountFactory internal factory;
+    address internal implementation;
+
+    // copy here the event definition from the contract
+    // @dev: once we bump to 0.8.21, import the event from the contract
+    event AccountCreated(bytes32 loginHash, address account);
+
+    function setUp() external {
+        factory = new AccountFactory(address(0), address(0), address(0));
+        implementation = factory.accountImplementation();
+    }
+
+    function test_ShouldNeverRevert(bytes32 randomHash) external {
+        // it should never revert
+
+        try factory.createAccount(randomHash) {
+            assertTrue(true);
+        } catch Error(string memory) {
+            fail("factory.constructor() reverted");
+        } catch {
+            fail("factory.constructor() reverted");
+        }
+    }
+
+    function test_ShouldUseADeterministicDeploymentProcess() external {
+        // predict where the account linked to a specific hash will be deployed
+        bytes32 loginHash = keccak256("muffin");
+        address predictedAddress = factory.getAddress(loginHash);
+
+        // check the address of the account doesn't have any code before the deployment
+        assertEq(keccak256(predictedAddress.code), keccak256(""));
+
+        // deploy the account contract using the same hash
+        factory.createAccount(loginHash);
+
+        // make sure the account contract has been deployed
+        assertNotEq(keccak256(predictedAddress.code), keccak256(""));
+    }
+
+    function test_GivenAHashAlreadyUsed() external {
+        // it should return existing account address
+
+        bytes32 loginHash = keccak256("bao");
+
+        // make sure the second attempt of creation return the already deployed address
+        assertEq(factory.createAccount(loginHash), factory.createAccount(loginHash));
+    }
+
+    function test_GivenANewHash(bytes32 loginHash1, bytes32 loginHash2) external {
+        // it should deploy a new account if none exists
+
+        vm.assume(loginHash1 != loginHash2);
+        assertNotEq(factory.createAccount(loginHash1), factory.createAccount(loginHash2));
+    }
+
+    function test_ShouldCallInitializeAfterDeployment() external {
+        bytes32 loginHash = keccak256("plplplplplplplpl");
+
+        // we tell the VM to expect *one* call to the initialize function with the loginHash as parameter
+        vm.expectCall(implementation, abi.encodeCall(this.initialize, (loginHash)), 1);
+
+        // we call the function that is supposed to trigger the call
+        factory.createAccount(loginHash);
+    }
+
+    function test_ShouldTriggerAnEventOnDeployment() external {
+        bytes32 loginHash = keccak256("event");
+
+        // we tell the VM to expect an event
+        vm.expectEmit(true, true, false, true, address(factory));
+        // we trigger the exact event we expect to be emitted in the next call
+        emit AccountCreated(loginHash, factory.getAddress(loginHash));
+
+        // we call the function that is supposed to trigger the event
+        // if the exact event is not triggered, the test will fail
+        factory.createAccount(loginHash);
+    }
+
+    // @dev: I don't know why but encodeCall crashes when using Account.initialize
+    //       when using the utils Test contract from Forge, so I had to copy the function here
+    //       it works as expected if I switch to the utils Test contract from PRB 🤷‍♂️
+    //       Anyway, remove this useless function once the bug is fixed
+    function initialize(bytes32) public { }
+}

4dc0d76da06453a2e0e9f7c201cb9521f3efe4b9
qd-qd <qdqdqdqdqd@protonmail.com>
2023-11-30 15:16:21 +0100

    🔨 make BaseScript more generic/powerful
    This light refactoring of BaseScript makes it more generic and powerful.
    Based on the flag you provide when running the script, it will either
    start the hardware wallet flow or use the private key you provide.
    
    In addition, I added new comments for each script to detail how to use
    them.

--- script/AccountFactory/CreateAccount.s.sol
+++ script/AccountFactory/CreateAccount.s.sol
@@ -5,14 +5,26 @@ import { AccountFactory } from "src/AccountFactory.sol";
 import { BaseScript } from "../Base.s.sol";
 
 /// @title Create an Account using an already deployed AccountFactory
-/**
- * @notice
- *  forge script CreateAccount --sig "run(address,bytes32)" <...args>  <...flags>
- */
 /// @dev If you need to deploy an AccountFactory, use the Deploy script in this directory
 contract CreateAccount is BaseScript {
-    function run(address factoryAddress, bytes32 loginHash) public broadcaster returns (address) {
+    function run(address factoryAddress, bytes32 loginHash) public broadcast returns (address) {
         AccountFactory factory = AccountFactory(factoryAddress);
         return factory.createAccount(loginHash);
     }
 }
+
+/*
+
+    ℹ️ HOW TO USE THIS SCRIPT USING A LEDGER:
+    forge script CreateAccount --rpc-url <RPC_URL> --ledger --sender <ACCOUNT_ADDRESS>  [--broadcast]
+
+
+    ℹ️ HOW TO USE THIS SCRIPT WITH AN ARBITRARY PRIVATE KEY (NOT RECOMMENDED):
+    PRIVATE_KEY=<PRIVATE_KEY> forge script CreateAccount --rpc-url <RPC_URL> [--broadcast]
+
+
+    ℹ️ HOW TO USE THIS SCRIPT ON ANVIL IN DEFAULT MODE:
+    forge script CreateAccount --rpc-url http://127.0.0.1:8545 --broadcast --sender \
+    0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --mnemonics "test test test test test test test test test test test junk"
+
+*/

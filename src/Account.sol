4d4188182a10c96bdf6de4e382f66b785390eabc
qd-qd <qdqdqdqdqd@protonmail.com>
2024-01-11 19:17:18 +0100

    ✨ Develop the addFirstSigner function
    This commit includes the function used to add the first and only the first
    signer of the account.
    In addition to that, this commit incudes a new function in the TestBase
    contract to bound a coordinate on the secp256r1 curve.

--- src/Account.sol
+++ src/Account.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.20 <0.9.0;
 import { IEntryPoint } from "@eth-infinitism/interfaces/IEntryPoint.sol";
 import { Initializable } from "@openzeppelin/proxy/utils/Initializable.sol";
 import { StorageSlotRegistry } from "src/StorageSlotRegistry.sol";
+import { SignerVaultWebAuthnP256R1 } from "src/SignerVaultWebAuthnP256R1.sol";
 
 contract Account is Initializable {
     // ==============================
@@ -11,6 +12,27 @@ contract Account is Initializable {
     // ==============================
     IEntryPoint public immutable entryPoint;
     address public immutable webAuthnVerifier;
+
+    // ==============================
+    // ========== EVENTS ============
+    // ==============================
+
+    /// @notice Emitted every time a signer is added to the account
+    /// @dev The credIdHash is indexed to allow off-chain services to track account with same signer authorized
+    event SignerAdded(bytes32 indexed credIdHash, uint256 pubKeyX, uint256 pubKeyY);
+
+    // ==============================
+    // ========== ERRORS ============
+    // ==============================
+
+    /// @notice This error is thrown if `firstSignerFuse` is set to false. That can happen if:
+    ///         - `addFirstSigner` is called before calling the `initialize` function
+    ///         - `firstSignerFuse` has already been called in the past
+    error FirstSignerAlreadySet();
+
+    // ==============================
+    // ======= CONSTRUCTION =========
+    // ==============================
 
     /// @notice Called by the factory at contrusction time when it deploys the account
     /// @dev    Do not store any state in this function as the contract will be proxified, only immutable variables
@@ -32,6 +54,57 @@ contract Account is Initializable {
             sstore(slot, 1)
         }
         }
+
+    // ==============================
+    // ======== FUNCTIONS ===========
+    // ==============================
+
+    /// @notice This modifier check if the fuse has already been burnt and revert the transaction if it is the case
+    ///         if the fuse has not been burnt yet, it burns it and allow the function to be called
+    /// @dev    The fuse is stored at the slot given by the constant `StorageSlotRegistry.FIRST_SIGNER_FUSE`
+    modifier singleUseLock() {
+        bytes32 slotFirstSignerFuse = StorageSlotRegistry.FIRST_SIGNER_FUSE;
+        bool currentFuseValue;
+
+        // check the value of the fuse variable
+        assembly ("memory-safe") {
+            currentFuseValue := sload(slotFirstSignerFuse)
+        }
+
+        // if the fuse has already been burnt (set to false), revert the transaction
+        if (currentFuseValue == false) revert FirstSignerAlreadySet();
+
+        // burn the fuse to prevent this function to be called again in the future
+        assembly ("memory-safe") {
+            sstore(slotFirstSignerFuse, 0)
+        }
+
+        // continue the execution of the function
+        _;
+    }
+
+    /// @notice Add the first signer to the account. This function is only call once by the factory
+    ///         during the deployment of the account. All the futures signers must be added using the
+    ///         `addSigner` function.
+    /// @dev    This function is expected to add a signer generated using the WebAuthn protocol on the
+    ///         secp256r1 curve. Adding another type of signer as the first signer is not supported yet.
+    ///
+    ///         As the call of this function is expected to be wrapped in the same transaction than a
+    ///         interaction with the account, we do not check webauthn's payload yet.
+    ///         The payload is automatically check in the execution function meaning if the payload
+    ///         is incorrect or do not correspond to the signer stored in this function, the whole tx
+    ///         will revert (reverting de facto the signer stored in this function)
+    /// @param  pubkeyX The X coordinate of the signer's public key.
+    /// @param  pubkeyY The Y coordinate of the signer's public key.
+    /// @param  credId The credential ID associated to the signer
+    function addFirstSigner(uint256 pubkeyX, uint256 pubkeyY, bytes memory credId) external singleUseLock {
+        // the length of the credId is unpredictable (cf webauthn length), that's why we hash it
+        bytes32 credIdHash = keccak256(credId);
+
+        // add account's first signer and emit the signer addition event
+        SignerVaultWebAuthnP256R1.set(credIdHash, pubkeyX, pubkeyY);
+        emit SignerAdded(credIdHash, pubkeyX, pubkeyY);
+    }
     }
 
 // ==============================
---
+++ test/unit/Account/addFirstSigner.t.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: APACHE-2.0
+pragma solidity >=0.8.20 <0.9.0;
+
+import { Account as SmartAccount } from "src/Account.sol";
+import { SignerVaultWebAuthnP256R1 } from "src/SignerVaultWebAuthnP256R1.sol";
+import { BaseTest } from "test/BaseTest.sol";
+import { StorageSlotRegistry } from "src/StorageSlotRegistry.sol";
+
+contract Account__AddFirstSigner is BaseTest {
+    SmartAccount private account;
+    uint256 private pubkeyX = 0x1;
+    uint256 private pubkeyY = 0x2;
+
+    // Duplicate of the event in the Account.sol file
+    event SignerAdded(bytes32 indexed credIdHash, uint256 pubKeyX, uint256 pubKeyY);
+
+    function setUp() external {
+        // deploy the account
+        account = new SmartAccount(address(1), address(2));
+    }
+
+    function test_RevertsIfTheFuseIsNotSet() external {
+        // it reverts if the fuse is not set
+
+        vm.expectRevert(abi.encodeWithSelector(SmartAccount.FirstSignerAlreadySet.selector));
+
+        account.addFirstSigner(pubkeyX, pubkeyY, "qdqdqdqdqddqd");
+    }
+
+    function test_BurnsTheFuse() external {
+        // it burns the fuse
+
+        // initialize the account to set the fuse to true
+        account.initialize();
+
+        // make sure the fuse is set to true
+        assertEq(vm.load(address(account), StorageSlotRegistry.FIRST_SIGNER_FUSE), bytes32(uint256(1)));
+
+        // burn the fuse by adding the first signer
+        account.addFirstSigner(pubkeyX, pubkeyY, "qdqdqdqdqddqd");
+
+        // make sure the fuse is set to false
+        assertEq(vm.load(address(account), StorageSlotRegistry.FIRST_SIGNER_FUSE), bytes32(0));
+    }
+
+    function test_CanNotBeCalledTwice() external {
+        // initialize the account to set the fuse to true
+        account.initialize();
+
+        // burn the fuse by adding the first signer
+        account.addFirstSigner(pubkeyX, pubkeyY, "qdqdqdqdqddqd");
+
+        // expect an error for the next call of `addFirstSigner`
+        vm.expectRevert(abi.encodeWithSelector(SmartAccount.FirstSignerAlreadySet.selector));
+
+        // try to add the first signer again -- this must revert
+        account.addFirstSigner(pubkeyX, pubkeyY, "qdqdqdqdqddqd");
+    }
+
+    function test_StoresTheSigner() external {
+        // it stores the signer
+
+        bytes memory credId = "qdqdqdqdqddqd";
+        bytes32 credIdHash = keccak256(credId);
+
+        // initialize the account to set the fuse to true
+        account.initialize();
+
+        // get the starting slot of the signer
+        bytes32 startingSlot = SignerVaultWebAuthnP256R1.getSignerStartingSlot(credIdHash);
+
+        // check nothing has been stored yet
+        assertEq(vm.load(address(account), startingSlot), bytes32(0));
+        assertEq(bytes32(uint256(vm.load(address(account), bytes32(uint256(startingSlot) + 1)))), bytes32(0));
+        assertEq(bytes32(uint256(vm.load(address(account), bytes32(uint256(startingSlot) + 2)))), bytes32(0));
+
+        // add the first signer
+        account.addFirstSigner(pubkeyX, pubkeyY, credId);
+
+        // check the signer has been stored
+        assertEq(vm.load(address(account), startingSlot), credIdHash);
+        assertEq(uint256(vm.load(address(account), bytes32(uint256(startingSlot) + 1))), pubkeyX);
+        assertEq(uint256(vm.load(address(account), bytes32(uint256(startingSlot) + 2))), pubkeyY);
+    }
+
+    function test_EmitsTheSignerAddEvent(uint256 fuzzedPubKeyX, uint256 fuzzedPubKeyY) external {
+        // it emits the signer add event
+
+        // bound the pubkey to the secp256r1 curve
+        fuzzedPubKeyX = boundP256R1(fuzzedPubKeyX);
+        fuzzedPubKeyY = boundP256R1(fuzzedPubKeyY);
+
+        bytes memory credId = "qdqdqdqdqddqd";
+        bytes32 credIdHash = keccak256(credId);
+
+        // initialize the account to set the fuse to true
+        account.initialize();
+
+        // we tell the VM to expect an event
+        vm.expectEmit(true, true, true, true, address(account));
+
+        // we trigger the exact event we expect to be emitted in the next call
+        emit SignerAdded(credIdHash, fuzzedPubKeyX, fuzzedPubKeyY);
+
+        // burn the fuse by adding the first signer
+        account.addFirstSigner(fuzzedPubKeyX, fuzzedPubKeyY, credId);
+    }
+}

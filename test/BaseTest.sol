8e7142320f4c663582fcd7f4058b7f9d8e40bbbe
qd-qd <qdqdqdqdqd@protonmail.com>
2024-01-11 19:17:18 +0100

    ✨ Develop the addFirstSigner function
    This commit includes the function used to add the first and only the first
    signer of the account.
    In addition to that, this commit incudes a new function in the TestBase
    contract to bound a coordinate on the secp256r1 curve.

--- test/BaseTest.sol
+++ test/BaseTest.sol
@@ -4,9 +4,16 @@ pragma solidity >=0.8.20 <0.9.0;
 import { Test } from "forge-std/Test.sol";
 
 contract BaseTest is Test {
+    uint256 constant P256R1_MAX =
+        115_792_089_210_356_248_762_697_446_949_407_573_530_086_143_415_290_314_195_533_631_308_867_097_853_951;
+
     modifier assumeNoPrecompile(address fuzzedAddress) {
         assumeNotPrecompile(fuzzedAddress);
 
         _;
+    }
+
+    function boundP256R1(uint256 x) internal pure returns (uint256) {
+        return x % P256R1_MAX;
     }
 }

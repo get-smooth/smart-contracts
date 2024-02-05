53a2a99a5127a0ad9165f2b57d57be8d75ba037f
qd-qd <qdqdqdqdqd@protonmail.com>
2024-02-05 21:27:58 +0100

    ♻️ improve the signature verification flow
    This commit improves the verification of the signature of the name
    service by using the real data.

--- test/unit/AccountFactory/createAndInitAccount.t.sol
+++ test/unit/AccountFactory/createAndInitAccount.t.sol
@@ -5,10 +5,6 @@ import { AccountFactory } from "src/AccountFactory.sol";
 import { BaseTest } from "test/BaseTest.sol";
 
 contract AccountFactory__CreateAndInitAccount is BaseTest {
-    bytes32 private constant LOGIN_HASH = keccak256("qdqd");
-    address private constant SIGNER = 0x7a8c35e1CcE64FD85baeD9a3e4f399cAADb52f20;
-    bytes private constant SIGNATURE = hex"247bbb60d4e8fd56e177234fb566331249f367465120c95ce65f"
-        hex"a784b0b917cd6e19a4b6ebfb5d93a217ea76c37ff6d98d5f3aa18015e7220543a95d215a50381c";
     AccountFactory private factory;
 
     // copy here the event definition from the contract
@@ -18,18 +14,20 @@ contract AccountFactory__CreateAndInitAccount is BaseTest {
     );
 
     function setUp() external {
-        factory = new AccountFactory(address(0), address(0), SIGNER);
+        factory = new AccountFactory(address(0), address(0), validCreate.signer);
     }
 
     function test_UseADeterministicDeploymentProcess() external {
         // predict where the account linked to a specific hash will be deployed
-        address predictedAddress = factory.getAddress(LOGIN_HASH);
+        address predictedAddress = factory.getAddress(validCreate.loginHash);
 
         // check the address of the account doesn't have any code before the deployment
         assertEq(keccak256(predictedAddress.code), keccak256(""));
 
         // deploy the account contract using the same hash
-        factory.createAndInitAccount(uint256(0), uint256(0), LOGIN_HASH, hex"", SIGNATURE);
+        factory.createAndInitAccount(
+            validCreate.pubKeyX, validCreate.pubKeyY, validCreate.loginHash, validCreate.credId, validCreate.signature
+        );
 
         // make sure the account contract has been deployed
         assertNotEq(keccak256(predictedAddress.code), keccak256(""));
@@ -41,8 +39,20 @@ contract AccountFactory__CreateAndInitAccount is BaseTest {
         // make sure the second attempt of creation return the already deployed address
         // without reverting or something else
         assertEq(
-            factory.createAndInitAccount(uint256(0), uint256(0), LOGIN_HASH, hex"", SIGNATURE),
-            factory.createAndInitAccount(uint256(0), uint256(0), LOGIN_HASH, hex"", SIGNATURE)
+            factory.createAndInitAccount(
+                validCreate.pubKeyX,
+                validCreate.pubKeyY,
+                validCreate.loginHash,
+                validCreate.credId,
+                validCreate.signature
+            ),
+            factory.createAndInitAccount(
+                validCreate.pubKeyX,
+                validCreate.pubKeyY,
+                validCreate.loginHash,
+                validCreate.credId,
+                validCreate.signature
+            )
         );
     }
 
@@ -50,21 +60,11 @@ contract AccountFactory__CreateAndInitAccount is BaseTest {
         // it should deploy a new account if none exists
 
         // deploy a valid proxy account using the constants predefined
-        address proxy1 = factory.createAndInitAccount(uint256(0), uint256(0), LOGIN_HASH, hex"", SIGNATURE);
+        address proxy1 = factory.createAndInitAccount(
+            validCreate.pubKeyX, validCreate.pubKeyY, validCreate.loginHash, validCreate.credId, validCreate.signature
+        );
 
-        // generated using the same private key as the one used to generate the SIGNATURE constant
-        bytes memory newValidCorrectSignature =
-            hex"4b4b6f4ecc5fb0427bbbe61b539a6c45062b45d794641a5dc86e12bd8c6f68a747"
-            hex"df2abe57a1df59f2d2d5ba5f9c89d723ea8a7c1ca79e95fc1321f3eeb775f51c";
-        bytes32 newLoginHash = keccak256("xoxo");
-
-        // deploy a valid proxy account using a different loginHash and a correct valid signature
-        address proxy2 =
-            factory.createAndInitAccount(uint256(0), uint256(0), newLoginHash, hex"", newValidCorrectSignature);
-
-        assertNotEq(proxy1, proxy2);
         assertNotEq(keccak256(proxy1.code), keccak256(""));
-        assertNotEq(keccak256(proxy2.code), keccak256(""));
     }
 
     function test_RevertWithAnIncorrectValidSignature() external {
@@ -76,11 +76,15 @@ contract AccountFactory__CreateAndInitAccount is BaseTest {
 
         // we tell the VM to expect a revert with a precise error
         vm.expectRevert(
-            abi.encodeWithSelector(AccountFactory.InvalidNameServiceSignature.selector, LOGIN_HASH, invalidSignature)
+            abi.encodeWithSelector(
+                AccountFactory.InvalidNameServiceSignature.selector, validCreate.loginHash, invalidSignature
+            )
         );
 
         // we call the function with the invalid signature to trigger the error
-        factory.createAndInitAccount(uint256(0), uint256(0), LOGIN_HASH, hex"", invalidSignature);
+        factory.createAndInitAccount(
+            validCreate.pubKeyX, validCreate.pubKeyY, validCreate.loginHash, validCreate.credId, invalidSignature
+        );
     }
 
     function test_CallInitialize() external {
@@ -88,38 +92,44 @@ contract AccountFactory__CreateAndInitAccount is BaseTest {
         vm.expectCall(factory.accountImplementation(), abi.encodeWithSelector(this.initialize.selector), 1);
 
         // we call the function that is supposed to trigger the call
-        factory.createAndInitAccount(uint256(0), uint256(0), LOGIN_HASH, hex"", SIGNATURE);
+        factory.createAndInitAccount(
+            validCreate.pubKeyX, validCreate.pubKeyY, validCreate.loginHash, validCreate.credId, validCreate.signature
+        );
     }
 
     function test_CallTheProxyAddFirstSignerFunction() external {
-        uint256 pubKeyX = uint256(43);
-        uint256 pubKeyY = uint256(22);
-        bytes memory credId = abi.encodePacked(keccak256("a"), keccak256("b"));
-
         // we tell the VM to expect *one* call to the addFirstSigner function with the loginHash as parameter
         vm.expectCall(
-            factory.getAddress(LOGIN_HASH),
-            abi.encodeCall(this.addFirstSigner, (pubKeyX, pubKeyY, keccak256(credId))),
+            factory.getAddress(validCreate.loginHash),
+            abi.encodeCall(
+                this.addFirstSigner, (validCreate.pubKeyX, validCreate.pubKeyY, keccak256(validCreate.credId))
+            ),
             1
         );
 
         // we call the function that is supposed to trigger the call
-        factory.createAndInitAccount(pubKeyX, pubKeyY, LOGIN_HASH, credId, SIGNATURE);
+        factory.createAndInitAccount(
+            validCreate.pubKeyX, validCreate.pubKeyY, validCreate.loginHash, validCreate.credId, validCreate.signature
+        );
     }
 
     function test_TriggerAnEventOnDeployment() external {
-        uint256 pubKeyX = uint256(43);
-        uint256 pubKeyY = uint256(22);
-        bytes memory credId = abi.encodePacked(keccak256("a"), keccak256("b"));
-
         // we tell the VM to expect an event
         vm.expectEmit(true, true, true, true, address(factory));
         // we trigger the exact event we expect to be emitted in the next call
-        emit AccountCreated(LOGIN_HASH, factory.getAddress(LOGIN_HASH), keccak256(credId), pubKeyX, pubKeyY);
+        emit AccountCreated(
+            validCreate.loginHash,
+            factory.getAddress(validCreate.loginHash),
+            keccak256(validCreate.credId),
+            validCreate.pubKeyX,
+            validCreate.pubKeyY
+        );
 
         // we call the function that is supposed to trigger the event
         // if the exact event is not triggered, the test will fail
-        factory.createAndInitAccount(pubKeyX, pubKeyY, LOGIN_HASH, credId, SIGNATURE);
+        factory.createAndInitAccount(
+            validCreate.pubKeyX, validCreate.pubKeyY, validCreate.loginHash, validCreate.credId, validCreate.signature
+        );
     }
 
     // @dev: I don't know why but encodeCall crashes when using Account.XXX

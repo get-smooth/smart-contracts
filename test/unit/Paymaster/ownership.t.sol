3808c9018ce6785a7c3e28703e5439c3ad589d01
qd-qd <qdqdqdqdqd@protonmail.com>
2024-02-07 23:49:05 +0100

    ✅ add unit tests for paymaster

---
+++ test/unit/Paymaster/ownership.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.8.20 <0.9.0;
+
+import { Paymaster, OwnershipTransferNotAllowed } from "src/Paymaster.sol";
+import { BaseTest } from "test/BaseTest.sol";
+
+contract Paymaster__Ownership is BaseTest {
+    address immutable admin = makeAddr("admin");
+    address immutable notAdmin = makeAddr("notAdmin");
+    address immutable entrypoint = makeAddr("entrypoint");
+
+    Paymaster private paymaster;
+
+    function setUp() external {
+        paymaster = new Paymaster(entrypoint, admin);
+    }
+
+    function test_RevertOnOwnershipTransfer() external {
+        // it revert on ownership transfer
+
+        // try to call transferOwnership with the admin address -- expect revert
+        vm.prank(admin);
+        vm.expectRevert(abi.encodeWithSelector(OwnershipTransferNotAllowed.selector));
+        paymaster.transferOwnership(notAdmin);
+
+        // try to call transferOwnership with an non admin address -- expect revert
+        vm.prank(notAdmin);
+        vm.expectRevert(abi.encodeWithSelector(OwnershipTransferNotAllowed.selector));
+        paymaster.transferOwnership(notAdmin);
+    }
+
+    function test_RevertOnOwnershipRenouncement() external {
+        // it revert on ownership renouncement
+
+        // try to call renounceOwnership with the admin address -- expect revert
+        vm.prank(admin);
+        vm.expectRevert(abi.encodeWithSelector(OwnershipTransferNotAllowed.selector));
+        paymaster.renounceOwnership();
+    }
+}

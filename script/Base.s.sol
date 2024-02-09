433c58ad91abe51cd61d0625b096ad11f12cc37c
qd-qd <qdqdqdqdqd@protonmail.com>
2024-02-09 11:37:52 +0100

    🔨 write paymaster scripts
    This commit contains a set of scripts to deploy, deposit and withdraw
    from a paymaster contract.

--- script/Base.s.sol
+++ script/Base.s.sol
@@ -4,6 +4,9 @@ pragma solidity >=0.8.19 <0.9.0;
 import { Script } from "forge-std/Script.sol";
 
 abstract contract BaseScript is Script {
+    // Current universal address of the 4337 entrypoint
+    address internal constant DEFAULT_ENTRYPOINT = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+
     /// @notice this modifier can be used as a generic broadcast solution. It will automatically either:
     ///        - use the private key provided as an environment variable to broadcast
     ///        - or starts the hardware wallet flow if the correct flags are provided and the env variable is not set
---
+++ script/Paymaster/02_PaymasterDeposit.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: APACHE-2.0
+pragma solidity >=0.8.19 <0.9.0;
+
+import { Paymaster } from "src/Paymaster.sol";
+import { BaseScript } from "../Base.s.sol";
+
+/// @title  PaymasterDeposit
+/// @notice Deposit funds for the paymaster
+contract PaymasterDeposit is BaseScript {
+    function run() public payable broadcast {
+        address paymasterAddress = vm.envAddress("PAYMASTER");
+        uint256 value = vm.envUint("AMOUNT");
+
+        Paymaster paymaster = Paymaster(paymasterAddress);
+
+        // get the initial balance of the paymaster
+        uint256 initialBalance = paymaster.getDeposit();
+
+        // deposit some funds into the paymaster
+        paymaster.deposit{ value: value }();
+
+        // check that the deposit was successful
+        require(paymaster.getDeposit() == initialBalance + value, "Deposit failed");
+    }
+}
+
+/*
+    ℹ️ HOW TO USE THIS SCRIPT USING A LEDGER:
+    ADMIN=<ADMIN_ADDRESS> forge script PaymasterDeposit --rpc-url <RPC_URL> --ledger  \
+    --sender <ACCOUNT_ADDRESS>  [--broadcast]
+
+
+    ℹ️ HOW TO USE THIS SCRIPT WITH AN ARBITRARY PRIVATE KEY (NOT RECOMMENDED):
+    PRIVATE_KEY=<PRIVATE_KEY> ADMIN=<ADMIN_ADDRESS>  forge script PaymasterDeposit  \
+    --rpc-url <RPC_URL>[--broadcast]
+
+
+    ℹ️ HOW TO USE THIS SCRIPT ON ANVIL IN DEFAULT MODE:
+    ADMIN=<ADMIN_ADDRESS>  forge script PaymasterDeposit --rpc-url http://127.0.0.1:8545 --broadcast --sender \
+    0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --mnemonics "test test test test test test test test test test test junk"
+*/

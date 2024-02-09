86b3fbb26a084fc927eb6a86821ef0afc8e43dd4
qd-qd <qdqdqdqdqd@protonmail.com>
2024-02-09 13:02:59 +0100

    ♻️ rewrite/improve factory scripts
    There are now 3 scripts for the AccountFactory:
    - 01_FactoryDeploy.s.sol to deploy the factory
    - 02_FactoryCreateAndInitAccount.s.sol to create and init an account
    - 03_FactoryGetAddress.s.sol to compute the future address of an account

---
+++ script/AccountFactory/01_FactoryDeploy.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: APACHE-2.0
+pragma solidity >=0.8.19 <0.9.0;
+
+import { AccountFactory } from "src/AccountFactory.sol";
+import { BaseScript } from "../Base.s.sol";
+
+/// @title  FactoryDeploy
+/// @notice Deploy the account factory
+contract FactoryDeploy is BaseScript {
+    function run() public broadcast returns (AccountFactory) {
+        address admin = vm.envAddress("ADMIN");
+        address webAuthnVerifier = vm.envAddress("WEBAUTHN_VERIFIER");
+        address entrypoint = vm.envOr("ENTRYPOINT", DEFAULT_ENTRYPOINT);
+
+        return new AccountFactory(entrypoint, webAuthnVerifier, admin);
+    }
+}
---
+++ script/AccountFactory/03_FactoryGetAddress.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: APACHE-2.0
+pragma solidity >=0.8.19 <0.9.0;
+
+import { AccountFactory } from "src/AccountFactory.sol";
+import { BaseScript } from "../Base.s.sol";
+
+/// @title  FactoryGetAddress
+/// @notice Compute the address of the account based on the loginHash
+contract FactoryGetAddress is BaseScript {
+    function run() public broadcast returns (address accountAddress) {
+        // address of the factory we wanna use
+        address factoryAddress = vm.envAddress("FACTORY");
+        AccountFactory factory = AccountFactory(factoryAddress);
+
+        // arguments to pass to the `createAndInit` function
+        bytes32 loginHash = vm.envBytes32("LOGIN_HASH");
+
+        // check the account is not already deployed
+        accountAddress = factory.getAddress(loginHash);
+    }
+}

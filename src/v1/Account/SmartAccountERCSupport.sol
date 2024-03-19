7ab8d62e55026e00b43f8c7505f4614acb4e64cb
qd-qd <qdqdqdqdqd@protonmail.com>
2024-03-19 16:07:05 +0100

    ✨ SmartAccount: Support for ERC1155 transfers
    Add the functions `onERC1155Received`, `onERC1155BatchReceived`
    and the correct response when calling `supportsInterface` that
    are required when the SmartAccount want to mint/trasnfer some
    ERC1155 tokens.

--- src/v1/Account/SmartAccountERCSupport.sol
+++ src/v1/Account/SmartAccountERCSupport.sol
@@ -2,6 +2,8 @@
 pragma solidity >=0.8.20 <0.9.0;
 
 import { IERC721Receiver } from "@openzeppelin/token/ERC721/IERC721Receiver.sol";
+import { IERC1155Receiver } from "@openzeppelin/token/ERC1155/IERC1155Receiver.sol";
+import { IERC165 } from "@openzeppelin/utils/introspection/IERC165.sol";
 
 /// @title ERC721Support
 /// @notice Indicate the contract supports ERC721 token transfer
@@ -27,8 +29,72 @@ contract ERC721Support is IERC721Receiver {
     }
 }
 
+/// @title ERC1155Support
+/// @notice Indicate the contract supports ERC1155 token transfer
+abstract contract ERC1155Support is IERC1155Receiver {
+    /// @notice Indicate that the contract supports ERC1155 token transfer
+    /// @param {operator} The address which called `safeTransferFrom` function
+    /// @param {from} The address which are tokens transferred from
+    /// @param {id} The token identifier
+    /// @param {value} The amount of tokens transferred
+    /// @param {data} Additional data with no specified format
+    /// @return `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
+    function onERC1155Received(
+        address, // operator
+        address, // from
+        uint256, // id
+        uint256, // value
+        bytes calldata // data
+    )
+        external
+        pure
+        override
+        returns (bytes4)
+    {
+        return this.onERC1155Received.selector;
+    }
+
+    /// @notice Indicate that the contract supports ERC1155 token batch transfer
+    /// @param {operator} The address which called `safeBatchTransferFrom` function
+    /// @param {from} The address which are tokens transferred from
+    /// @param {ids} An array of token identifiers
+    /// @param {values} An array of the amount of tokens transferred
+    /// @param {data} Additional data with no specified format
+    /// @return `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`
+    function onERC1155BatchReceived(
+        address, // operator
+        address, // from
+        uint256[] calldata, // ids
+        uint256[] calldata, // values
+        bytes calldata // data
+    )
+        external
+        pure
+        override
+        returns (bytes4)
+    {
+        return this.onERC1155BatchReceived.selector;
+    }
+}
+
+/// @title ERC165Support
+/// @notice Implement the `supportsInterface` function according to ERC-165
+contract ERC165Support is IERC165 {
+    // ERC-165 identifier for the `ERC1155TokenReceiver` support
+    bytes4 private constant ERC165_ERC1155_TOKENRECEIVER = 0x4e2312e0;
+    // ERC-165 support (i.e. `bytes4(keccak256('supportsInterface(bytes4)'))`).
+    bytes4 private constant ERC165_STANDARD = 0x01ffc9a7;
+
+    /// @notice Indicates that the contract supports the ERC-165 and ERC1155 standards
+    /// @dev The ERC721 standard doesn't have a specific interface ID
+    /// @param interfaceID The interface identifier, as specified in ERC-165
+    /// @return `true` if the contract implements `interfaceID`
+    function supportsInterface(bytes4 interfaceID) public pure returns (bool) {
+        return interfaceID == ERC165_STANDARD || interfaceID == ERC165_ERC1155_TOKENRECEIVER;
+    }
+}
+
 /// @title SmartAccountERCSupport
 /// @dev Inherit the SmartAccount contract with this contract tokens transfer
 // solhint-disable-next-line no-empty-blocks
-contract SmartAccountERCSupport is ERC721Support { }
-
+contract SmartAccountERCSupport is ERC721Support, ERC165Support, ERC1155Support { }
---
+++ test/unit/v1/Account/SmartAccountERCSupport/ERC1155Support.t.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: APACHE-2.0
+pragma solidity >=0.8.20 <0.9.0;
+
+import { ERC1155 } from "@openzeppelin/token/ERC1155/ERC1155.sol";
+import { SmartAccount } from "src/v1/Account/SmartAccount.sol";
+import { BaseTest } from "test/BaseTest.sol";
+
+contract SmartAccountERCSupport__ERC1155 is BaseTest {
+    address internal entrypoint;
+    SmartAccount internal account;
+    DumbERC1155 internal erc1155;
+
+    address internal immutable initialOwner = makeAddr("initial_owner");
+
+    function setUp() external {
+        // 1. deploy a new instance of the account
+        entrypoint = makeAddr("entrypoint");
+        account = new SmartAccount(entrypoint, makeAddr("verifier"));
+
+        // 2. deploy an ER1155 token and mint some tokens to the initial owner
+        erc1155 = new DumbERC1155();
+        erc1155.mint(initialOwner, 1, 200, hex"");
+        erc1155.mint(initialOwner, 2, 100, hex"2233");
+    }
+
+    function test_CanReceiveERC1155Tokens() external {
+        // it can receive ERC1155 tokens
+
+        // 1. transfer tokens from the first collection to the account
+        vm.prank(initialOwner);
+        erc1155.safeTransferFrom(initialOwner, address(account), 1, 10, hex"");
+
+        // 2. transfer tokens from the second collection to the account
+        vm.prank(initialOwner);
+        erc1155.safeTransferFrom(initialOwner, address(account), 2, 20, hex"");
+
+        // 3. mint some tokens directly to the account
+        vm.prank(initialOwner);
+        erc1155.mint(address(account), 3, 30, hex"");
+
+        // 4. make sure the tokens are transferred
+        assertEq(erc1155.balanceOf(address(account), 1), 10);
+        assertEq(erc1155.balanceOf(address(account), 2), 20);
+        assertEq(erc1155.balanceOf(address(account), 3), 30);
+    }
+
+    function test_CanApproveAllERC1155Tokens() external {
+        // it can approve all ERC1155 tokens
+
+        // 1. transfer some tokens from the first collection to the account
+        vm.prank(initialOwner);
+        erc1155.safeTransferFrom(initialOwner, address(account), 1, 10, hex"");
+
+        // 2. approve all the tokens from the first collection to another address
+        address to = makeAddr("another_address");
+        vm.prank(entrypoint);
+        account.execute(address(erc1155), 0, abi.encodeWithSelector(erc1155.setApprovalForAll.selector, to, true));
+
+        // 3. make sure the tokens are approved
+        assert(erc1155.isApprovedForAll(address(account), to));
+    }
+
+    function test_CanTransferfromERC20Tokens() external {
+        // it can transferfrom ERC20 tokens
+
+        uint256 amount = 10;
+
+        // 1. transfer some tokens from the first collection to the account
+        vm.prank(initialOwner);
+        erc1155.safeTransferFrom(initialOwner, address(account), 1, amount, hex"");
+
+        // 2. transfer the tokens from the first collection to another address
+        address to = makeAddr("another_address");
+        vm.prank(entrypoint);
+        account.execute(
+            address(erc1155),
+            0,
+            abi.encodeWithSelector(erc1155.safeTransferFrom.selector, address(account), to, 1, amount, hex"ddee")
+        );
+
+        // 3. make sure the tokens are transferred
+        assertEq(erc1155.balanceOf(address(account), 1), 0);
+        assertEq(erc1155.balanceOf(to, 1), amount);
+    }
+
+    // @DEV: dynamic-length array must be in the storage to avoid a 0x41 EVM error when dealing with mock1155
+    uint256[] internal ids = [uint256(1), uint256(2)];
+    uint256[] internal values = [10, 20];
+
+    function test_CanBatchTransferFromERC20Tokens() external {
+        // it can batch transferFrom ERC20 tokens
+
+        // 1. transfer some tokens from the first and second collection to the account
+        vm.prank(initialOwner);
+        erc1155.safeTransferFrom(initialOwner, address(account), 1, values[0], hex"");
+        vm.prank(initialOwner);
+        erc1155.safeTransferFrom(initialOwner, address(account), 2, values[1], hex"");
+
+        // 2. batch transfer the tokens
+        address to = makeAddr("another_address");
+        vm.prank(entrypoint);
+        account.execute(
+            address(erc1155),
+            0,
+            abi.encodeWithSelector(erc1155.safeBatchTransferFrom.selector, address(account), to, ids, values, hex"")
+        );
+
+        // 3. make sure the tokens are transferred
+        assertEq(erc1155.balanceOf(address(account), ids[0]), 0);
+        assertEq(erc1155.balanceOf(address(account), ids[1]), 0);
+        assertEq(erc1155.balanceOf(to, ids[0]), values[0]);
+        assertEq(erc1155.balanceOf(to, ids[1]), values[1]);
+    }
+}
+
+contract DumbERC1155 is ERC1155 {
+    constructor() ERC1155("https://token-cdn-domain/{id}.json") { }
+
+    function mint(address to, uint256 id, uint256 value, bytes memory data) external {
+        _mint(to, id, value, data);
+    }
+}

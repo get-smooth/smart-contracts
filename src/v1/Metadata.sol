e3afccb7e6ec2e666ce5bb29018e543deb2fddac
qd-qd <qdqdqdqdqd@protonmail.com>
2024-03-29 12:12:47 +0100

    🔨 better framework for the entrypoint in scripts
    Instead of allowing an arbitrary address to be passed as the entrypoint
    to the scripts, we now have a constant `ENTRYPOINT` in the scripts that
    is set to the address of the `Metadata` contract. This is less error
    prone.

--- src/v1/Metadata.sol
+++ src/v1/Metadata.sol
@@ -3,4 +3,16 @@ pragma solidity >=0.8.20 <0.9.0;
 
 library Metadata {
     string internal constant VERSION = "1.0.0";
+
+    // default address -- entrypoint version: 0.6.0
+    address internal constant ENTRYPOINT_ADDRESS = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+
+    // entrypoint version: 0.7.0
+    address internal constant ENTRYPOINT_ADDRESS_V0_7 = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
+
+    /// @notice Get the version of the entrypoint to use
+    /// @dev The current default version of the entrypoint is 0.6.0
+    function entrypoint() internal pure returns (address) {
+        return ENTRYPOINT_ADDRESS;
+    }
 }

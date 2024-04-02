751c934c80d240342e9d0681d28ab9e53fd4d058
qd-qd <qdqdqdqdqd@protonmail.com>
2024-04-02 19:48:23 +0200

    🔨 paymaster: get operator script

---
+++ script/Paymaster/07_PaymasterGetOperator.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: APACHE-2.0
+pragma solidity >=0.8.19 <0.9.0;
+
+import { Paymaster } from "src/v1/Paymaster.sol";
+import { BaseScript } from "../Base.s.sol";
+
+/// @title  PaymasterGetOperator
+/// @notice Fetch the operator of the paymaster
+contract PaymasterGetOperator is BaseScript {
+    function run() public broadcast returns (address operator) {
+        // address of the paymaster we wanna use
+        address paymasterAddress = vm.envAddress("PAYMASTER");
+        Paymaster paymaster = Paymaster(paymasterAddress);
+
+        // fetch the operator of the paymaster
+        operator = paymaster.operator();
+    }
+}
